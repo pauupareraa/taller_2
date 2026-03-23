f31976332a28ca72b617eb9dec27dfbe6da82265
pauupareraa <pauparerabonet@gmail.com>
2026-03-23 12:45:46 +0100

    comandes

---
+++ comandes.sh
@@ -0,0 +1,19 @@
+mkdir documents
+cd documents/
+mkdir Complexos
+mkdir Inequacions
+cd ../
+cp exercici2.tex Complexos/
+cp exercici1.tex Inequacions/
+cd documents/
+cd Complexos/
+mv exercici2.tex solucio_2.tex
+cd ../
+cd Inequacions/
+mv exercici1.tex solucio_1.tex
+cd ../
+cd ../
+mkdir imatges
+cd imatges/
+touch info_imatge.txt
+echo "La imatge ha estat descarregada de texample.net" >> info_imatge.txt
